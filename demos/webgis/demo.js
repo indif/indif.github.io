c77c1c9d3292bb2c602f7c1712d0de599f4fa017
任利伟 <renlw@glodon.com>
2019-09-18 10:11:28 +0800

    天地图数据不可访问，使用bingmap数据

--- demos/webgis/demo.js
+++ demos/webgis/demo.js
@@ -19,30 +19,30 @@ var options = {
     }),
 
     // Bing地图影像服务
-    // imageryProvider: new Cesium.BingMapsImageryProvider({
-    // url: 'https://dev.virtualearth.net',
-    // key: 'ApBGk4ITPkE3SNeeE-2EIpW103i0fInzPnKS0K4rT5RrW-v9WnLSFT80caL96zh3',
-    // mapStyle: Cesium.BingMapsStyle.AERIAL
-    // }),
+    imageryProvider: new Cesium.BingMapsImageryProvider({
+    url: 'https://dev.virtualearth.net',
+    key: 'ApBGk4ITPkE3SNeeE-2EIpW103i0fInzPnKS0K4rT5RrW-v9WnLSFT80caL96zh3',
+    mapStyle: Cesium.BingMapsStyle.AERIAL
+    }),
 
     // 天地图全球影像服务
-    imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
-        url: "https://{s}.tianditu.com/img_c/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=c&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles",
-        layer: "img",
-        style: "default",
-        format: "tiles",
-        tileMatrixSetID: "c",
-        credit: new Cesium.Credit("天地图全球影像服务"),
-        subdomains: ['t0', 't1', 't2', 't3', 't4', 't5', 't6', 't7'],
-        maximumLevel: 18,
-        tilingScheme: new Cesium.GeographicTilingScheme(),
-        tileMatrixLabels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19'],
-        tileDiscardPolicy: new Cesium.DiscardMissingTileImagePolicy({
-            missingImageUrl: "https://t0.tianditu.com/img_c/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=c&TileMatrix=19&TileRow=0&TileCol=0&style=default&format=tiles",
-            pixelsToCheck: [new Cesium.Cartesian2(0, 0), new Cesium.Cartesian2(120, 140), new Cesium.Cartesian2(130, 160), new Cesium.Cartesian2(200, 50), new Cesium.Cartesian2(200, 200)],
-            disableCheckIfAllPixelsAreTransparent: true
-        })
-    }),
+    // imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
+    //     url: "https://{s}.tianditu.com/img_c/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=c&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles",
+    //     layer: "img",
+    //     style: "default",
+    //     format: "tiles",
+    //     tileMatrixSetID: "c",
+    //     credit: new Cesium.Credit("天地图全球影像服务"),
+    //     subdomains: ['t0', 't1', 't2', 't3', 't4', 't5', 't6', 't7'],
+    //     maximumLevel: 18,
+    //     tilingScheme: new Cesium.GeographicTilingScheme(),
+    //     tileMatrixLabels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19'],
+    //     tileDiscardPolicy: new Cesium.DiscardMissingTileImagePolicy({
+    //         missingImageUrl: "https://t0.tianditu.com/img_c/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=c&TileMatrix=19&TileRow=0&TileCol=0&style=default&format=tiles",
+    //         pixelsToCheck: [new Cesium.Cartesian2(0, 0), new Cesium.Cartesian2(120, 140), new Cesium.Cartesian2(130, 160), new Cesium.Cartesian2(200, 50), new Cesium.Cartesian2(200, 200)],
+    //         disableCheckIfAllPixelsAreTransparent: true
+    //     })
+    // }),
 
     // STK Terrain Service
     // terrainProvider: new Cesium.CesiumTerrainProvider({
@@ -69,15 +69,15 @@ var viewer = new Cesium.Viewer('ViewerContainer', options);
 viewer.scene.globe.depthTestAgainstTerrain = true;
 
 // 全球影像中文注记服务（叠加图层）
-viewer.imageryLayers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
-    url: "https://{s}.tianditu.com/cia_c/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=cia&tileMatrixSet=c&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default.jpg",
-    layer: "cia",
-    style: "default",
-    format: "tiles",
-    tileMatrixSetID: "c",
-    maximumLevel: 18,
-    credit: new Cesium.Credit("全球影像中文注记服务"),
-    subdomains: ['t0', 't1', 't2', 't3', 't4', 't5', 't6', 't7'],
-    tilingScheme: new Cesium.GeographicTilingScheme(),
-    tileMatrixLabels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19']
-}));
+// viewer.imageryLayers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
+//     url: "https://{s}.tianditu.com/cia_c/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=cia&tileMatrixSet=c&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default.jpg",
+//     layer: "cia",
+//     style: "default",
+//     format: "tiles",
+//     tileMatrixSetID: "c",
+//     maximumLevel: 18,
+//     credit: new Cesium.Credit("全球影像中文注记服务"),
+//     subdomains: ['t0', 't1', 't2', 't3', 't4', 't5', 't6', 't7'],
+//     tilingScheme: new Cesium.GeographicTilingScheme(),
+//     tileMatrixLabels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19']
+// }));
